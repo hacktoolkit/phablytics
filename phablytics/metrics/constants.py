c24f46aad6a82126a1eef7004eacb5b5965d2706
Jonathan Tsai <jontsai@users.noreply.github.com>
2021-12-14 02:46:43 +0000

    Minor improvements to metrics explorer - consistently sets default value for interval to `week` (was: `quarter` (form) or `month` (UI)) - converts TaskMetric from `namedtuple` base class to `dataclass`

--- phablytics/metrics/constants.py
+++ phablytics/metrics/constants.py
@@ -1,2 +1,16 @@
 DATE_FORMAT_YMD = '%Y-%m-%d'
 DATE_FORMAT_MDY_SHORT = '%b %d, %Y'
+
+INTERVAL_OPTIONS = [
+    'week',
+    'month',
+    'quarter',
+]
+DEFAULT_INTERVAL_OPTION = 'week'
+
+INTERVAL_DAYS_MAP = {
+    'week': 7,
+    'month': 30,
+    'quarter': 90,
+}
+DEFAULT_INTERVAL_DAYS = INTERVAL_DAYS_MAP[DEFAULT_INTERVAL_OPTION]
